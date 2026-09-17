428361ccac2d85b10d85e39b5d184508471fa72e
Alexander Tibbets <alexander.tibbets@gmail.com>
2026-09-17 13:52:08 -0700

    Social smashup

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Provides offline capabilities and performance improvements
  */
 
-const STATIC_CACHE = 'static-v01625b2c';
+const STATIC_CACHE = 'static-v526b4cbe';
 
 const CRITICAL_ASSETS = [
   '/',
